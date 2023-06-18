3afd8add6a0b270728e627b5d7cb6a9b1fae315a
Rhdshfeyhccfu <130246853+Mykola1612@users.noreply.github.com>
2023-06-19 00:32:54 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import './js/modal-pop_up_leave.js';
 import './js/modal-pop-up2_explore.js';
 import './js/review/script.js';
 import './js/review/slick.min.js';
-import './js/swiper.js';
+import '/js/swiper.js';
