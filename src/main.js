92f732f9ee6ce989c15d1ae140b0383dfcc0cd90
OlehDribko <131261254+OlehDribko@users.noreply.github.com>
2023-06-18 20:35:32 +0300

    review

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import './js/menu.js';
 import './js/modal_buy_now.js';
 import './js/modal-pop_up_leave.js';
 import './js/modal-pop-up2_explore.js';
+import './js/review/script.js';
+import './js/review/slick.min.js';
