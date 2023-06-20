111d9d6c4d1b42247eb86274caebbade1946f5fb
Rhdshfeyhccfu <130246853+Mykola1612@users.noreply.github.com>
2023-06-20 23:36:32 +0300

    Summary

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import '/js/menu.js';
 import '/js/modal_buy_now.js';
 import '/js/modal-pop_up_leave.js';
 import '/js/modal-pop-up2_explore.js';
-import '/js/review/script.js';
-import '/js/review/slick.min.js';
 import '/js/swiper.js';
 import '/js/mobile-menu.js';
