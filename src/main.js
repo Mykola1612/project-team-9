6df3e8e85206da80283aa080945d8afdbfd99d83
KaloRem <bogdan.paslawskii@gmail.com>
2023-06-21 13:43:15 +0200

    v1.0

--- src/main.js
+++ src/main.js
@@ -1,7 +1,2 @@
-import '/js/modal-buy-now.js';
-import '/js/modal-pop_up_leave.js';
-import '/js/modal-pop-up2_explore.js';
+import '/js/all.js';
 import '/js/swiper.js';
-import '/js/mobile-menu.js';
-import '/js/sub-modal.js';
-import '/js/all-modal.js';
